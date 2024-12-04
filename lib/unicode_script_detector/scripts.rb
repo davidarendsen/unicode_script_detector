be27c02e93d55d5ea0fbbce7f17adddccc5f060a
David Arendsen <davidarendsen@hey.com>
2024-12-04 10:17:55 +0000

    Add much more scripts

--- lib/unicode_script_detector/scripts.rb
+++ lib/unicode_script_detector/scripts.rb
@@ -2,14 +2,29 @@ module UnicodeScriptDetector
   class Scripts
     LIST = [
       {
-        script: :Digit,
-        name: "Digit",
-        regex: /\d/,
-      },
-      {
         script: :Whitespace,
         name: "Whitespace",
-        regex: /\s/,
+        regex: /\s/
+      },
+      {
+        script: :Digit,
+        name: "Digit",
+        regex: /\d/
+      },
+      { 
+        script: :Adlam, 
+        name: "Adlam", 
+        regex: /\p{Adlam}/, 
+      },
+      { 
+        script: :Ahom, 
+        name: "Ahom", 
+        regex: /\p{Ahom}/, 
+      },
+      { 
+        script: :Anatolian_Hieroglyphs, 
+        name: "Anatolian_Hieroglyphs", 
+        regex: /\p{Anatolian_Hieroglyphs}/, 
       },
       { 
         script: :Arabic, 
@@ -22,6 +37,11 @@ module UnicodeScriptDetector
         regex: /\p{Armenian}/, 
       },
       { 
+        script: :Avestan, 
+        name: "Avestan", 
+        regex: /\p{Avestan}/, 
+      },
+      { 
         script: :Balinese, 
         name: "Balinese", 
         regex: /\p{Balinese}/, 
@@ -32,6 +52,11 @@ module UnicodeScriptDetector
         regex: /\p{Bamum}/, 
       },
       { 
+        script: :Bassa_Vah, 
+        name: "Bassa_Vah", 
+        regex: /\p{Bassa_Vah}/, 
+      },
+      { 
         script: :Batak, 
         name: "Batak", 
         regex: /\p{Batak}/, 
@@ -40,6 +65,11 @@ module UnicodeScriptDetector
         script: :Bengali, 
         name: "Bengali", 
         regex: /\p{Bengali}/, 
+      },
+      { 
+        script: :Bhaiksuki, 
+        name: "Bhaiksuki", 
+        regex: /\p{Bhaiksuki}/, 
       },
       { 
         script: :Bopomofo, 
@@ -77,6 +107,11 @@ module UnicodeScriptDetector
         regex: /\p{Carian}/, 
       },
       { 
+        script: :Caucasian_Albanian, 
+        name: "Caucasian_Albanian", 
+        regex: /\p{Caucasian_Albanian}/, 
+      },
+      { 
         script: :Chakma, 
         name: "Chakma", 
         regex: /\p{Chakma}/, 
@@ -90,6 +125,11 @@ module UnicodeScriptDetector
         script: :Cherokee, 
         name: "Cherokee", 
         regex: /\p{Cherokee}/, 
+      },
+      { 
+        script: :Chorasmian, 
+        name: "Chorasmian", 
+        regex: /\p{Chorasmian}/, 
       },
       { 
         script: :Coptic, 
@@ -107,6 +147,11 @@ module UnicodeScriptDetector
         regex: /\p{Cypriot}/, 
       },
       { 
+        script: :Cypro_Minoan, 
+        name: "Cypro_Minoan", 
+        regex: /\p{Cypro_Minoan}/, 
+      },
+      { 
         script: :Cyrillic, 
         name: "Cyrillic", 
         regex: /\p{Cyrillic}/, 
@@ -122,15 +167,41 @@ module UnicodeScriptDetector
         regex: /\p{Devanagari}/, 
       },
       { 
+        script: :Dives_Akuru, 
+        name: "Dives_Akuru", 
+        regex: /\p{Dives_Akuru}/, 
+      },
+      { 
+        script: :Dogra, 
+        name: "Dogra", 
+        regex: /\p{Dogra}/, 
+      },
+      { 
+        script: :Duployan, 
+        name: "Duployan", 
+        regex: /\p{Duployan}/, 
+      },
+      { 
         script: :Egyptian_Hieroglyphs, 
         name: "Egyptian_Hieroglyphs", 
         regex: /\p{Egyptian_Hieroglyphs}/, 
+      },
+      { 
+        script: :Elbasan, 
+        name: "Elbasan", 
+        regex: /\p{Elbasan}/, 
+      },
+      { 
+        script: :Elymaic, 
+        name: "Elymaic", 
+        regex: /\p{Elymaic}/, 
       },
       { 
         script: :Ethiopic, 
         name: "Ethiopic", 
         regex: /\p{Ethiopic}/, 
       },
+    
       { 
         script: :Georgian, 
         name: "Georgian", 
@@ -147,6 +218,11 @@ module UnicodeScriptDetector
         regex: /\p{Gothic}/, 
       },
       { 
+        script: :Grantha, 
+        name: "Grantha", 
+        regex: /\p{Grantha}/, 
+      },
+      { 
         script: :Greek, 
         name: "Greek", 
         regex: /\p{Greek}/, 
@@ -157,10 +233,16 @@ module UnicodeScriptDetector
         regex: /\p{Gujarati}/, 
       },
       { 
+        script: :Gunjala_Gondi, 
+        name: "Gunjala_Gondi", 
+        regex: /\p{Gunjala_Gondi}/, 
+      },
+      { 
         script: :Gurmukhi, 
         name: "Gurmukhi", 
         regex: /\p{Gurmukhi}/, 
       },
+    
       { 
         script: :Han, 
         name: "Han", 
@@ -172,9 +254,19 @@ module UnicodeScriptDetector
         regex: /\p{Hangul}/, 
       },
       { 
+        script: :Hanifi_Rohingya, 
+        name: "Hanifi_Rohingya", 
+        regex: /\p{Hanifi_Rohingya}/, 
+      },
+      { 
         script: :Hanunoo, 
         name: "Hanunoo", 
         regex: /\p{Hanunoo}/, 
+      },
+      { 
+        script: :Hatran, 
+        name: "Hatran", 
+        regex: /\p{Hatran}/, 
       },
       { 
         script: :Hebrew, 
@@ -226,6 +318,12 @@ module UnicodeScriptDetector
         name: "Katakana", 
         regex: /\p{Katakana}/, 
       },
+    
+      { 
+        script: :Kawi, 
+        name: "Kawi", 
+        regex: /\p{Kawi}/, 
+      },
       { 
         script: :Kayah_Li, 
         name: "Kayah_Li", 
@@ -237,10 +335,26 @@ module UnicodeScriptDetector
         regex: /\p{Kharoshthi}/, 
       },
       { 
+        script: :Khitan_Small_Script, 
+        name: "Khitan_Small_Script", 
+        regex: /\p{Khitan_Small_Script}/, 
+      },
+      { 
         script: :Khmer, 
         name: "Khmer", 
         regex: /\p{Khmer}/, 
       },
+      { 
+        script: :Khojki, 
+        name: "Khojki", 
+        regex: /\p{Khojki}/, 
+      },
+      { 
+        script: :Khudawadi, 
+        name: "Khudawadi", 
+        regex: /\p{Khudawadi}/, 
+      },
+    
       { 
         script: :Lao, 
         name: "Lao", 
@@ -262,9 +376,19 @@ module UnicodeScriptDetector
         regex: /\p{Limbu}/, 
       },
       { 
+        script: :Linear_A, 
+        name: "Linear_A", 
+        regex: /\p{Linear_A}/, 
+      },
+      { 
         script: :Linear_B, 
         name: "Linear_B", 
         regex: /\p{Linear_B}/, 
+      },
+      { 
+        script: :Lisu, 
+        name: "Lisu", 
+        regex: /\p{Lisu}/, 
       },
       { 
         script: :Lycian, 
@@ -277,6 +401,16 @@ module UnicodeScriptDetector
         regex: /\p{Lydian}/, 
       },
       { 
+        script: :Mahajani, 
+        name: "Mahajani", 
+        regex: /\p{Mahajani}/, 
+      },
+      { 
+        script: :Makasar, 
+        name: "Makasar", 
+        regex: /\p{Makasar}/, 
+      },
+      { 
         script: :Malayalam, 
         name: "Malayalam", 
         regex: /\p{Malayalam}/, 
@@ -287,9 +421,34 @@ module UnicodeScriptDetector
         regex: /\p{Mandaic}/, 
       },
       { 
+        script: :Manichaean, 
+        name: "Manichaean", 
+        regex: /\p{Manichaean}/, 
+      },
+      { 
+        script: :Marchen, 
+        name: "Marchen", 
+        regex: /\p{Marchen}/, 
+      },
+      { 
+        script: :Masaram_Gondi, 
+        name: "Masaram_Gondi", 
+        regex: /\p{Masaram_Gondi}/, 
+      },
+      { 
+        script: :Medefaidrin, 
+        name: "Medefaidrin", 
+        regex: /\p{Medefaidrin}/, 
+      },
+      { 
         script: :Meetei_Mayek, 
         name: "Meetei_Mayek", 
         regex: /\p{Meetei_Mayek}/, 
+      },
+      { 
+        script: :Mende_Kikakui, 
+        name: "Mende_Kikakui", 
+        regex: /\p{Mende_Kikakui}/, 
       },
       { 
         script: :Meroitic_Cursive, 
@@ -307,9 +466,24 @@ module UnicodeScriptDetector
         regex: /\p{Miao}/, 
       },
       { 
+        script: :Modi, 
+        name: "Modi", 
+        regex: /\p{Modi}/, 
+      },
+      { 
         script: :Mongolian, 
         name: "Mongolian", 
         regex: /\p{Mongolian}/, 
+      },
+      { 
+        script: :Mro, 
+        name: "Mro", 
+        regex: /\p{Mro}/, 
+      },
+      { 
+        script: :Multani, 
+        name: "Multani", 
+        regex: /\p{Multani}/, 
       },
       { 
         script: :Myanmar, 
@@ -317,14 +491,44 @@ module UnicodeScriptDetector
         regex: /\p{Myanmar}/, 
       },
       { 
+        script: :Nabataean, 
+        name: "Nabataean", 
+        regex: /\p{Nabataean}/, 
+      },
+      { 
+        script: :Nag_Mundari, 
+        name: "Nag_Mundari", 
+        regex: /\p{Nag_Mundari}/, 
+      },
+      { 
+        script: :Nandinagari, 
+        name: "Nandinagari", 
+        regex: /\p{Nandinagari}/, 
+      },
+      { 
         script: :New_Tai_Lue, 
         name: "New_Tai_Lue", 
         regex: /\p{New_Tai_Lue}/, 
       },
       { 
+        script: :Newa, 
+        name: "Newa", 
+        regex: /\p{Newa}/, 
+      },
+      { 
         script: :Nko, 
         name: "Nko", 
         regex: /\p{Nko}/, 
+      },
+      { 
+        script: :Nushu, 
+        name: "Nushu", 
+        regex: /\p{Nushu}/, 
+      },
+      { 
+        script: :Nyiakeng_Puachue_Hmong, 
+        name: "Nyiakeng_Puachue_Hmong", 
+        regex: /\p{Nyiakeng_Puachue_Hmong}/, 
       },
       { 
         script: :Ogham, 
@@ -336,15 +540,36 @@ module UnicodeScriptDetector
         name: "Ol_Chiki", 
         regex: /\p{Ol_Chiki}/, 
       },
+    
+      { 
+        script: :Old_Hungarian, 
+        name: "Old_Hungarian", 
+        regex: /\p{Old_Hungarian}/, 
+      },
       { 
         script: :Old_Italic, 
         name: "Old_Italic", 
         regex: /\p{Old_Italic}/, 
       },
       { 
+        script: :Old_North_Arabian, 
+        name: "Old_North_Arabian", 
+        regex: /\p{Old_North_Arabian}/, 
+      },
+      { 
+        script: :Old_Permic, 
+        name: "Old_Permic", 
+        regex: /\p{Old_Permic}/, 
+      },
+      { 
         script: :Old_Persian, 
         name: "Old_Persian", 
         regex: /\p{Old_Persian}/, 
+      },
+      { 
+        script: :Old_Sogdian, 
+        name: "Old_Sogdian", 
+        regex: /\p{Old_Sogdian}/, 
       },
       { 
         script: :Old_South_Arabian, 
@@ -357,14 +582,39 @@ module UnicodeScriptDetector
         regex: /\p{Old_Turkic}/, 
       },
       { 
+        script: :Old_Uyghur, 
+        name: "Old_Uyghur", 
+        regex: /\p{Old_Uyghur}/, 
+      },
+      { 
         script: :Oriya, 
         name: "Oriya", 
         regex: /\p{Oriya}/, 
       },
       { 
+        script: :Osage, 
+        name: "Osage", 
+        regex: /\p{Osage}/, 
+      },
+      { 
         script: :Osmanya, 
         name: "Osmanya", 
         regex: /\p{Osmanya}/, 
+      },
+      { 
+        script: :Pahawh_Hmong, 
+        name: "Pahawh_Hmong", 
+        regex: /\p{Pahawh_Hmong}/, 
+      },
+      { 
+        script: :Palmyrene, 
+        name: "Palmyrene", 
+        regex: /\p{Palmyrene}/, 
+      },
+      { 
+        script: :Pau_Cin_Hau, 
+        name: "Pau_Cin_Hau", 
+        regex: /\p{Pau_Cin_Hau}/, 
       },
       { 
         script: :Phags_Pa, 
@@ -377,6 +627,11 @@ module UnicodeScriptDetector
         regex: /\p{Phoenician}/, 
       },
       { 
+        script: :Psalter_Pahlavi, 
+        name: "Psalter_Pahlavi", 
+        regex: /\p{Psalter_Pahlavi}/, 
+      },
+      { 
         script: :Rejang, 
         name: "Rejang", 
         regex: /\p{Rejang}/, 
@@ -385,6 +640,11 @@ module UnicodeScriptDetector
         script: :Runic, 
         name: "Runic", 
         regex: /\p{Runic}/, 
+      },
+      { 
+        script: :Samaritan, 
+        name: "Samaritan", 
+        regex: /\p{Samaritan}/, 
       },
       { 
         script: :Saurashtra, 
@@ -402,9 +662,24 @@ module UnicodeScriptDetector
         regex: /\p{Shavian}/, 
       },
       { 
+        script: :Siddham, 
+        name: "Siddham", 
+        regex: /\p{Siddham}/, 
+      },
+      { 
+        script: :SignWriting, 
+        name: "SignWriting", 
+        regex: /\p{SignWriting}/, 
+      },
+      { 
         script: :Sinhala, 
         name: "Sinhala", 
         regex: /\p{Sinhala}/, 
+      },
+      { 
+        script: :Sogdian, 
+        name: "Sogdian", 
+        regex: /\p{Sogdian}/, 
       },
       { 
         script: :Sora_Sompeng, 
@@ -412,10 +687,16 @@ module UnicodeScriptDetector
         regex: /\p{Sora_Sompeng}/, 
       },
       { 
+        script: :Soyombo, 
+        name: "Soyombo", 
+        regex: /\p{Soyombo}/, 
+      },
+      { 
         script: :Sundanese, 
         name: "Sundanese", 
         regex: /\p{Sundanese}/, 
       },
+    
       { 
         script: :Syloti_Nagri, 
         name: "Syloti_Nagri", 
@@ -462,6 +743,16 @@ module UnicodeScriptDetector
         regex: /\p{Tamil}/, 
       },
       { 
+        script: :Tangsa, 
+        name: "Tangsa", 
+        regex: /\p{Tangsa}/, 
+      },
+      { 
+        script: :Tangut, 
+        name: "Tangut", 
+        regex: /\p{Tangut}/, 
+      },
+      { 
         script: :Telugu, 
         name: "Telugu", 
         regex: /\p{Telugu}/, 
@@ -487,9 +778,26 @@ module UnicodeScriptDetector
         regex: /\p{Tifinagh}/, 
       },
       { 
+        script: :Tirhuta, 
+        name: "Tirhuta", 
+        regex: /\p{Tirhuta}/, 
+      },
+    
+      { 
+        script: :Toto, 
+        name: "Toto", 
+        regex: /\p{Toto}/, 
+      },
+    
+      { 
         script: :Ugaritic, 
         name: "Ugaritic", 
         regex: /\p{Ugaritic}/, 
+      },
+      { 
+        script: :Unknown, 
+        name: "Unknown", 
+        regex: /\p{Unknown}/, 
       },
       { 
         script: :Vai, 
@@ -497,14 +805,39 @@ module UnicodeScriptDetector
         regex: /\p{Vai}/, 
       },
       { 
+        script: :Vithkuqi, 
+        name: "Vithkuqi", 
+        regex: /\p{Vithkuqi}/, 
+      },
+      { 
+        script: :Wancho, 
+        name: "Wancho", 
+        regex: /\p{Wancho}/, 
+      },
+      { 
+        script: :Warang_Citi, 
+        name: "Warang_Citi", 
+        regex: /\p{Warang_Citi}/, 
+      },
+      { 
+        script: :Yezidi, 
+        name: "Yezidi", 
+        regex: /\p{Yezidi}/, 
+      },
+      { 
         script: :Yi, 
         name: "Yi", 
         regex: /\p{Yi}/, 
       },
-      {
-        script: :Emoji,
-        name: "Emoji",
-        regex: /\p{Emoji}/,
+      { 
+        script: :Zanabazar_Square, 
+        name: "Zanabazar_Square", 
+        regex: /\p{Zanabazar_Square}/, 
+      },
+      { 
+        script: :Emoji, 
+        name: "Emoji", 
+        regex: /\p{Emoji}/, 
       },
       { 
         script: :Common, 
