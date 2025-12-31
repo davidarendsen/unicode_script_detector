9a2498545fd4e9a43197247d74f8400fce126d15
David Arendsen <davidarendsen@hey.com>
2025-12-31 13:33:20 +0100

    Add facade to script_groups method

--- lib/unicode_script_detector.rb
+++ lib/unicode_script_detector.rb
@@ -9,6 +9,10 @@ module UnicodeScriptDetector
       UnicodeScriptDetector::Detector.new(string).characters
     end
 
+    def script_groups(string)
+      UnicodeScriptDetector::Detector.new(string).script_groups
+    end
+
     def contains?(string, scripts)
       UnicodeScriptDetector::Detector.new(string).contains?(scripts)
     end
